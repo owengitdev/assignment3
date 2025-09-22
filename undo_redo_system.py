2c68695816d56ab3dc3c456cc1c21d1dde4f97b2
owengitdev <yanyida19790228@gmail.com>
2025-09-21 21:46:11 -0400

    Update undo_redo_system.py

--- undo_redo_system.py
+++ undo_redo_system.py
@@ -3,11 +3,39 @@ from node import Node
 
 # Implement your Stack class here
 class Stack:
-    pass # delete this line
+    def __init__(self):
+        self.top = None
+
+    def push(self,value):
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+
+    def pop(self):
+        if not self.top:
+            return None
+        removed_node = self.top
+        self.top = self.top.next
+        return removed_node.value
+
+    def peek(self):
+        if self.top:
+            return self.top.value
+        else:
+            return None
+
+    def print_stack(self):
+        current = self.top
+        if not current:
+            print("Stack is empty")
+            return
+        while current:
+            print(f"- {current.value}")
+            current = current.next
 
 def run_undo_redo():
     # Create instances of the Stack class for undo and redo
-    
+    my_stack = Stack()
 
     while True:
         print("\n--- Undo/Redo Manager ---")
@@ -22,18 +50,19 @@ def run_undo_redo():
         if choice == "1":
             action = input("Describe the action (e.g., Insert 'a'): ")
             # Push the action onto the undo stack and clear the redo stack
-
+            my_stack.push("Page 1")
+            my_stack.push("Page 2")
+            my_stack.push("Page 3")
 
             print(f"Action performed: {action}")
         elif choice == "2":
             # Pop an action from the undo stack and push it onto the redo stack
-            pass # delete this line
+            print(my_stack.pop())
             
 
         elif choice == "3":
             # Pop an action from the redo stack and push it onto the undo stack
-            pass # delete this line
-
+            print(my_stack.peek())
 
         elif choice == "4":
             # Print the undo stack
